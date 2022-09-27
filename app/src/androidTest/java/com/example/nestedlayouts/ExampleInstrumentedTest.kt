ad5e284b430e7b1acbf44d8868a93b4991f38ada
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2022-09-27 20:53:19 +0700

    27.09.22_1

--- app/src/androidTest/java/com/example/nestedlayouts/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/nestedlayouts/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package ru.kemsu.mobileapp
+package com.example.nestedlayouts
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ru.kemsu.mobileapp", appContext.packageName)
+        assertEquals("com.example.nestedlayouts", appContext.packageName)
     }
 }
