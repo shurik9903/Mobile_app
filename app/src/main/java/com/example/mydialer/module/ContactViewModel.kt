708f5ab077677054dda22f282ec0f3512572b26a
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2022-11-13 18:27:13 +0700

    13.11.22_3

--- app/src/main/java/com/example/mydialer/module/ContactViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mydialer.module
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.example.mydialer.data.Contact
-
-class ContactViewModel : ViewModel() {
-    var contactList = MutableLiveData<List<Contact>>()
-}
